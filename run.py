95555ab75d755715a82ae36ea27086149de40b25
Jake <jakemacnaughton@gmail.com>
2025-03-08 14:27:14 -0500

    created barebones of scraper

---
+++ run.py
@@ -0,0 +1,45 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+
+
+def scrape_website():
+    url = 'PCV_SEARCH_URL'
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.content, "html.parser")
+        data = extract_data(soup)
+        print(f"Scraped data at {datetime.now()}: {data}")
+        proces_data(data)
+    except requests.exceptions.RequestException as e:
+        print(f"Error during request: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def extract_data(soup):
+    # implement data extraction from soup logic here
+    import pdb; pdb.set_trace()
+
+def process_data(data):
+    # write to csv file here
+    import pdb; pdb.set_trace()
+    # with open("scraped_data.csv", "a") as file:
+        # file.write()
+
+def send_alert():
+    # send alert to my cell phone here
+    import pdb; pdb.set_trace()
+
+def run_scraper():
+    scrape_website()
+
+def run_scheduler():
+    today = datetime.today().date()
+    sched.add_job(run_scraper, 'interval', minutes=15, start_date=today.first_update_time(), end_date=today.crunch_time_update_start())
+
+if __name__ == "__main__":
+    while True:
+        run_scheduler()
