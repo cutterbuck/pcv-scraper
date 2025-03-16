759951da2afcfc142b9ee47f5e9a63bc33f34928
Jake <jakemacnaughton@gmail.com>
2025-03-16 14:11:53 -0400

    tryinbg one more thing with chrome

--- package/etl.py
+++ package/etl.py
@@ -15,9 +15,11 @@ def run_scraper():
     url = 'https://www.stuytown.com/nyc-apartments-for-rent?Order=low-price&PropertyName=Peter+Cooper+Village&Bedrooms=2&Flex=false&Bathrooms=2'
     try:
         s=Service(ChromeDriverManager().install())
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--headless') # ensure GUI is off
-        driver = webdriver.Chrome(service=s, options=chrome_options)
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless') # ensure GUI is off
+        options.add_argument("--no-sandbox")
+        options.headless = True
+        driver = webdriver.Chrome(service=s, options=options)
         driver.get(url)
         html = driver.page_source
         print('received html')
@@ -82,14 +84,14 @@ run_scraper()
 # GMT == NYC time +4
 def manage_scheduler(sched):
     today = datetime.today().date()
-    start_time = datetime(today.year, today.month, today.day, 18, 10, 0)
+    start_time = datetime(today.year, today.month, today.day, 18, 25, 0)
     end_time = datetime(today.year, today.month, today.day, 19, 10, 0)
     print("Resetting run_scraper for today")
     sched.add_job(run_scraper, 'interval', minutes=15, start_date=start_time, end_date=end_time)
 
 def run_scheduler():
     sched = BackgroundScheduler(daemon=True)
-    manage_jobs_trigger = CronTrigger(year="*", month="*", day="*", hour="18", minute="05", second="50")
+    manage_jobs_trigger = CronTrigger(year="*", month="*", day="*", hour="18", minute="20", second="50")
     sched.add_job(manage_scheduler, args=[sched], trigger=manage_jobs_trigger, start_date=datetime.now())
     print("Starting scheduler")
     sched.start()
