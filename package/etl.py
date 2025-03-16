0c2579d236bbf2d3d500f1c4581b400f985cd448
Jake <jakemacnaughton@gmail.com>
2025-03-16 16:21:11 -0400

    another try

--- package/etl.py
+++ package/etl.py
@@ -16,7 +16,7 @@ def run_scraper():
     try:
         options = webdriver.ChromeOptions()
         options.add_argument('--no-sandbox')
-        options.add_argument('--headless')
+        options.add_argument("--headless=new")
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument("start-maximized");
         options.add_argument("disable-infobars");
