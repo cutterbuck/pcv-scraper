b8b235a67eb04f171cf7646d16325126d116d6b6
Jake <jakemacnaughton@gmail.com>
2025-06-03 14:28:28 -0400

    requirements.txt refresh

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 from package.app import app
+from waitress import serve
 
 
 if __name__ == '__main__':
-    # from waitress import serve
     # serve(app.server, host="0.0.0.0", port=8080)
-
     app.run(debug=True, dev_tools_ui=True, use_reloader=False, port=8080)
