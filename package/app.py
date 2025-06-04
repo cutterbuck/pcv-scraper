d14eb5f8d46bec52715b52f1521791aca73cd43c
Jake <jakemacnaughton@gmail.com>
2025-06-04 11:38:11 -0400

    switched server

--- package/app.py
+++ package/app.py
@@ -3,5 +3,4 @@ import dash
 app = dash.Dash(__name__, url_base_pathname='/', title='PCV Apartments', update_title=None)
 app.server.config['DEBUG'] = True
 app.server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.server.app_context().push()
 app.config['suppress_callback_exceptions'] = True
