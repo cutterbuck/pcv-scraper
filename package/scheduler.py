37a0b58b9845fe7349a3bb10bd48d196c349468b
Jake <jakemacnaughton@gmail.com>
2025-06-05 14:40:22 -0400

    take 2

--- package/scheduler.py
+++ package/scheduler.py
@@ -12,12 +12,12 @@ def manage_scheduler(sched):
     print("Resetting scheduler for today")
     # update_trackers()
     today = datetime.today().date()
-    start_time = datetime(today.year, today.month, today.day, 14, 25, 0).astimezone(ZoneInfo('America/New_York'))
-    end_time = datetime(today.year, today.month, today.day, 14, 59, 0).astimezone(ZoneInfo('America/New_York'))
+    start_time = datetime(today.year, today.month, today.day, 14, 55, 0).astimezone(ZoneInfo('America/New_York'))
+    end_time = datetime(today.year, today.month, today.day, 15, 59, 0).astimezone(ZoneInfo('America/New_York'))
     sched.add_job(test_schedules, 'interval', minutes=5, start_date=start_time, end_date=end_time)
 
 def run_scheduler():
     sched = BackgroundScheduler(daemon=True)
-    manage_jobs_trigger = CronTrigger(year="*", month="*", day="*", hour="13", minute="20", second="0")
+    manage_jobs_trigger = CronTrigger(year="*", month="*", day="*", hour="14", minute="50", second="0")
     sched.add_job(manage_scheduler, args=[sched], trigger=manage_jobs_trigger, start_date=datetime.now().astimezone(ZoneInfo('America/New_York')))
     sched.start()
