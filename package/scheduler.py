7583655fb209fd39f5bdda31167dd1c7cb8b0f2a
Jake <jakemacnaughton@gmail.com>
2025-06-05 15:16:22 -0400

    trying utc time

--- package/scheduler.py
+++ package/scheduler.py
@@ -12,12 +12,13 @@ def manage_scheduler(sched):
     print("Resetting scheduler jobs")
     # update_trackers()
     today = datetime.today().date()
-    start_time = datetime(today.year, today.month, today.day, 15, 15, 0).astimezone(ZoneInfo('America/New_York'))
-    end_time = datetime(today.year, today.month, today.day, 16, 59, 0).astimezone(ZoneInfo('America/New_York'))
+    start_time = datetime(today.year, today.month, today.day, 19, 25, 0).astimezone(ZoneInfo('UTC'))
+    end_time = datetime(today.year, today.month, today.day, 20, 59, 0).astimezone(ZoneInfo('UTC'))
     sched.add_job(test_schedules, 'interval', minutes=5, start_date=start_time, end_date=end_time)
 
 def run_scheduler():
+    print("Starting scheduler")
     sched = BackgroundScheduler(daemon=True)
-    manage_jobs_trigger = CronTrigger(year="*", month="*", day="*", hour="15", minute="10", second="0")
-    sched.add_job(manage_scheduler, args=[sched], trigger=manage_jobs_trigger, start_date=datetime.now().astimezone(ZoneInfo('America/New_York')))
+    manage_jobs_trigger = CronTrigger(year="*", month="*", day="*", hour="19", minute="20", second="0")
+    sched.add_job(manage_scheduler, args=[sched], trigger=manage_jobs_trigger, start_date=datetime.now().astimezone(ZoneInfo('UTC')))
     sched.start()
