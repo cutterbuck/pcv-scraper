96ff5bc333d19672f02eab4b76674b3043f0423d
Jake <jakemacnaughton@gmail.com>
2025-06-05 16:52:24 -0400

    changed timer to 15 min, set scraper to early morning

--- package/scheduler.py
+++ package/scheduler.py
@@ -5,20 +5,18 @@ from package.dash_layout import update_trackers
 from zoneinfo import ZoneInfo
 
 
-def test_schedules():
-    print("Hello world!")
 
 def manage_scheduler(sched):
     print("Resetting scheduler jobs")
     today = datetime.today().date()
-    start_time = datetime(today.year, today.month, today.day, 20, 45, 0).astimezone(ZoneInfo('UTC'))
-    end_time = datetime(today.year, today.month, today.day, 22, 0, 0).astimezone(ZoneInfo('UTC'))
-    sched.add_job(update_trackers, 'interval', minutes=5, start_date=start_time, end_date=end_time)
+    start_time = datetime(today.year, today.month, today.day, 7, 0, 0).astimezone(ZoneInfo('UTC'))
+    end_time = datetime(today.year, today.month, today.day, 10, 0, 0).astimezone(ZoneInfo('UTC'))
+    sched.add_job(update_trackers, 'interval', minutes=15, start_date=start_time, end_date=end_time)
 
 def run_scheduler():
     print("Starting scheduler")
     update_trackers()
     sched = BackgroundScheduler(daemon=True)
-    manage_jobs_trigger = CronTrigger(year="*", month="*", day="*", hour="20", minute="41", second="0")
+    manage_jobs_trigger = CronTrigger(year="*", month="*", day="*", hour="6", minute="59", second="0")
     sched.add_job(manage_scheduler, args=[sched], trigger=manage_jobs_trigger, start_date=datetime.now().astimezone(ZoneInfo('UTC')))
     sched.start()
