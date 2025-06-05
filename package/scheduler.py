fcbf403d27eca331dbb644a871924e0a6546e63b
Jake <jakemacnaughton@gmail.com>
2025-06-05 16:09:08 -0400

    adding back scrapes

--- package/scheduler.py
+++ package/scheduler.py
@@ -1,7 +1,7 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
 from datetime import datetime
-# from package.dash_layout import update_trackers
+from package.dash_layout import update_trackers
 from zoneinfo import ZoneInfo
 
 
@@ -10,15 +10,15 @@ def test_schedules():
 
 def manage_scheduler(sched):
     print("Resetting scheduler jobs")
-    # update_trackers()
+    update_trackers()
     today = datetime.today().date()
-    start_time = datetime(today.year, today.month, today.day, 19, 25, 0).astimezone(ZoneInfo('UTC'))
+    start_time = datetime(today.year, today.month, today.day, 20, 20, 0).astimezone(ZoneInfo('UTC'))
     end_time = datetime(today.year, today.month, today.day, 20, 59, 0).astimezone(ZoneInfo('UTC'))
-    sched.add_job(test_schedules, 'interval', minutes=5, start_date=start_time, end_date=end_time)
+    sched.add_job(update_trackers, 'interval', minutes=5, start_date=start_time, end_date=end_time)
 
 def run_scheduler():
     print("Starting scheduler")
     sched = BackgroundScheduler(daemon=True)
-    manage_jobs_trigger = CronTrigger(year="*", month="*", day="*", hour="19", minute="20", second="0")
+    manage_jobs_trigger = CronTrigger(year="*", month="*", day="*", hour="20", minute="15", second="0")
     sched.add_job(manage_scheduler, args=[sched], trigger=manage_jobs_trigger, start_date=datetime.now().astimezone(ZoneInfo('UTC')))
     sched.start()
