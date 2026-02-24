fbbf1538cd0edc1514a0db334026964d08c949f9
Jake <jakemacnaughton@gmail.com>
2026-02-24 12:49:47 -0500

    updating callbacks for quicker rerender

--- package/dash_layout.py
+++ package/dash_layout.py
@@ -1,4 +1,4 @@
-from dash import html, dcc, Input, Output, State, callback, dash_table, no_update
+from dash import html, dcc, Input, Output, State, callback, ctx, dash_table, no_update
 from package.app import app
 from package.scrape import run_scraper
 
@@ -6,17 +6,18 @@ data = []
 scrape_time = None
 
 
-@callback(Output('apt-listings-table', 'data'), Output('scrape-time-monitor', 'children'), Input('interval-component', 'n_intervals'), Input('refresh-button', 'n_clicks'), State('scrape-time-monitor', 'children'))
+@callback(Output('apt-listings-table', 'data'), Output('scrape-time-monitor', 'children'), Output('refresh-button', 'disabled'), Input('interval-component', 'n_intervals'), Input('refresh-button', 'n_clicks'), State('scrape-time-monitor', 'children'))
 def update_store(n_intervals, n_clicks, memory_scrape_time):
-    if n_clicks:
+    if ctx.triggered_id == 'refresh-button' and n_clicks:
         update_trackers()
+        return data, scrape_time, False
     if scrape_time is None:
         return no_update
     if memory_scrape_time != scrape_time:
         print("Inside callback. New scrape exists")
         print('memory_scrape_time', memory_scrape_time)
         print('scrape_time', scrape_time)
-        return data, scrape_time
+        return data, scrape_time, False
     else:
         return no_update
 
@@ -55,6 +56,13 @@ def update_trackers(alert=False):
     global data, scrape_time
     data, scrape_time = run_scraper(alert=alert)
 
+app.clientside_callback(
+    "function(n_clicks) { if (n_clicks > 0) { return true; } return window.dash_clientside.no_update; }",
+    Output('refresh-button', 'disabled', allow_duplicate=True),
+    Input('refresh-button', 'n_clicks'),
+    prevent_initial_call=True
+)
+
 app.layout = html.Div(id='table-wrapper', style={'width': '80%', 'marginLeft': '8%', 'marginTop': '4%'}, children=[
                 html.H3('Current Peter Cooper Village 2 Bedroom 2 Bathroom Listings'),
                 html.Div(children=[
@@ -62,6 +70,10 @@ app.layout = html.Div(id='table-wrapper', style={'width': '80%', 'marginLeft': '
                     html.P(id='scrape-time-monitor', style={'width': '20%', 'display': 'inline-block', 'marginTop': '0px'})
                 ]),
                 html.Button('Refresh', id='refresh-button', n_clicks=0),
-                generate_table(),
-                dcc.Interval(id='interval-component', interval=60000)
+                dcc.Loading(
+                    id='loading-indicator',
+                    type='default',
+                    children=generate_table()
+                ),
+                dcc.Interval(id='interval-component', interval=5000)
             ])
